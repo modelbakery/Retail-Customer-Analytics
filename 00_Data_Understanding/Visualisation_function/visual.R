beb9ca1e5eae4b9460725b923d0446652de99911
seunghyunsung <sung9741@gmail.com>
2022-02-10 00:32:52 +0900

    Data Preparation: Product Segmentation Customer Segementation

---
+++ 00_Data_Understanding/Visualisation_function/visual.R
@@ -0,0 +1,233 @@
+#==============================================================================|
+# Visualisation Plot 
+#==============================================================================|
+# The following plots are stored in here:
+# 1. Cummulative Plot: Useful for filtering out row elements by cumulative sum 
+#    -> removing long tail. 
+# 2. Lollipop Plot: Convey more information than the bar plot 
+# 3. Heatmap Plot: Great for showing details in 3 dimensions 
+#    -> correlation analysis 
+
+
+# 1.1. Get Cumulative Frequency Table 
+# -- Format data to cumulative Stats 
+# -- Label Priority row by setting the maximum cumulative frequency 
+# -- Add text label for visualisation 
+get_rank_cumulative <-
+function(data, .cumulative_max = 0.5){
+    out_tbl <- data %>% 
+        arrange(desc(n)) %>% 
+        mutate(
+            pct = n/ sum(n),
+            cumulative_pct = cumsum(pct),
+            priority = ifelse(cumulative_pct <= .cumulative_max, TRUE, FALSE)
+        ) %>% 
+        rowid_to_column(var = "rank") %>% 
+        mutate(label_text = str_glue("Rank: {rank}
+                                 Count: {n}
+                                 Pct: {scales::percent(pct)}
+                                 cumulative Pct: {scales::percent(cumulative_pct)}"))
+    return(out_tbl)
+}
+# 1.2. Cumulative Curve Plot 
+# -- Set maxmium slicing index 
+# -- plotly embedded 
+plot_lollipop <-
+function(data, X_var = terms, Y_var = pct, point_size = n,
+                          fct_reorder = FALSE, fct_rev = FALSE, facet_wrap = FALSE){
+    
+    X_expr <- enquo(X_var)
+    Y_expr <- enquo(Y_var)
+    
+    # Factor Ordering 
+    if(fct_reorder){
+        data <- data %>% 
+            mutate(
+                terms = terms %>% as_factor() %>% fct_reorder(!! Y_expr)
+                )
+    }
+    if(fct_rev){
+        data <- data %>% 
+            mutate(
+                terms = terms %>% as_factor() %>% fct_reorder(!! Y_expr)
+                )
+    }
+    
+    point_size_expr <- enquo(point_size)
+    
+    # Geometrics
+    g <- data %>% 
+        ggplot(aes(!! X_expr, !! Y_expr)) +
+        geom_segment(aes(xend = !! X_expr, yend = 0), size = 0.5) +
+        geom_point(aes(size = !! point_size_expr)) +
+        coord_flip() +
+        theme_tq() +
+        theme(
+            legend.position = "none",
+            plot.title = element_text(face = "bold"),
+            plot.caption = element_text(face = "bold.italic")
+        )
+    
+    return(g)
+}
+# 2. Lollipop Plot
+plot_lollipop <-
+function(data, X_var = terms, Y_var = pct, point_size = n,
+                          fct_reorder = FALSE, fct_rev = FALSE, facet_wrap = FALSE){
+    
+    X_expr <- enquo(X_var)
+    Y_expr <- enquo(Y_var)
+    
+    # Factor Ordering 
+    if(fct_reorder){
+        data <- data %>% 
+            mutate(
+                terms = terms %>% as_factor() %>% fct_reorder(!! Y_expr)
+                )
+    }
+    if(fct_rev){
+        data <- data %>% 
+            mutate(
+                terms = terms %>% as_factor() %>% fct_reorder(!! Y_expr)
+                )
+    }
+    
+    point_size_expr <- enquo(point_size)
+    
+    # Geometrics
+    g <- data %>% 
+        ggplot(aes(!! X_expr, !! Y_expr)) +
+        geom_segment(aes(xend = !! X_expr, yend = 0), size = 0.5) +
+        geom_point(aes(size = !! point_size_expr)) +
+        coord_flip() +
+        theme_tq() +
+        theme(
+            legend.position = "none",
+            plot.title = element_text(face = "bold"),
+            plot.caption = element_text(face = "bold.italic")
+        )
+    
+    return(g)
+}
+
+# plot_hist_facet.R ----
+# -- Plot Faceted Histogram Function 
+
+# Operations:
+## -- Generate Faceted Histogram to all type of data
+
+plot_hist_facet <- function(data, bins = 10,  ncol = 5, 
+                            fct_reorder = FALSE, fct_rev = FALSE, 
+                            fill = palette_light()[[3]], 
+                            color = "white", scale = "free") {
+    
+    data_factored <- data %>%
+        # To operate factor as well as character features 
+        mutate_if(is.character, as.factor) %>%
+        # Convert to numeric data to operate binning 
+        mutate_if(is.factor, as.numeric) %>%
+        # Long format to make facets  
+        # factor_key = TRUE to store key values as a factor 
+        # -- Preserve original ordering of the columns 
+        gather(key = key, value = value, factor_key = TRUE) 
+    
+    if (fct_reorder) {
+        # Output Features in alphabetically order 
+        # as.character() -> as.factor() arranges factors alphabetically  
+        data_factored <- data_factored %>%
+            mutate(key = as.character(key) %>% as.factor())
+    }
+    
+    if (fct_rev) {
+        # Output Features in a reverse alphabetically order 
+        data_factored <- data_factored %>%
+            mutate(key = fct_rev(key))
+    }
+    
+    g <- data_factored %>%
+        ggplot(aes(x = value, group = key)) +
+        geom_histogram(bins = bins, fill = fill, color = color) +
+        facet_wrap(~ key, ncol = ncol, scale = scale) + 
+        theme_tq()
+    
+    return(g)
+    
+}
+
+# Correlation_Function.R ----
+# -- get_cor
+# -- plot_cor
+
+# quoo() and enquo() do the exact same thing. The only difference 
+# is that quo() is used outside of functions and enquo() is used 
+# inside.
+
+# pairwise.complete.obs:
+# -- Arugment of cor(). Not the default, but is almost always what 
+# you want to do. If you use = "everything" (default), then you run the 
+# risk of getting errors from missing values. 
+# -- Any NA values will be ignored 
+
+get_cor <- function(data, target, use = "pairwise.complete.obs",
+                    fct_reorder = FALSE, fct_rev = FALSE) {
+    
+    feature_expr <- enquo(target)
+    feature_name <- quo_name(feature_expr)
+    
+    data_cor <- data %>% 
+        mutate_if(is.character, as.factor) %>% 
+        mutate_if(is.factor, as.numeric) %>% 
+        cor(use = use) %>% 
+        as.tibble() %>% 
+        mutate(feature = names(.)) %>% 
+        select(feature, !! feature_expr) %>% 
+        # remove target var row -> 1.00 correlation/not informative 
+        filter(!(feature == feature_name)) %>% 
+        mutate_if(is.character, as_factor)
+    
+    if(fct_reorder){
+        data_cor <- data_cor %>% 
+            mutate(feature = fct_reorder(feature, !! feature_expr)) %>% 
+            arrange(feature)
+    }
+    
+    if(fct_rev){
+        data_cor <- data_cor %>% 
+            mutate(feature = fct_rev(feature, !! feature_expr)) %>% 
+            arrange(feature)
+    }
+    return(data_cor)
+}
+
+
+plot_cor <- function(data, target, fct_reorder = FALSE, fct_rev = FALSE, 
+                     include_lbl = TRUE, lbl_precision = 2, lbl_position = "outward",
+                     size = 2, line_size = 1, vert_size = 1, 
+                     color_pos = palette_light()[[1]], 
+                     color_neg = palette_light()[[2]]) {
+    
+    feature_expr <- enquo(target)
+    feature_name <- quo_name(feature_expr)
+    
+    data_cor <- data %>%
+        get_cor(!! feature_expr, fct_reorder = fct_reorder, fct_rev = fct_rev) %>%
+        mutate(feature_name_text = round(!! feature_expr, lbl_precision)) %>%
+        mutate(Correlation = case_when(
+            (!! feature_expr) >= 0 ~ "Positive",
+            TRUE                   ~ "Negative") %>% as.factor())
+    
+    g <- data_cor %>%
+        ggplot(aes_string(x = feature_name, y = "feature", group = "feature")) +
+        geom_point(aes(color = Correlation), size = size) +
+        geom_segment(aes(xend = 0, yend = feature, color = Correlation), size = line_size) +
+        geom_vline(xintercept = 0, color = palette_light()[[1]], size = vert_size) +
+        expand_limits(x = c(-1, 1)) +
+        theme_tq() +
+        scale_color_manual(values = c(color_neg, color_pos)) 
+    
+    if (include_lbl) g <- g + geom_label(aes(label = feature_name_text), hjust = lbl_position)
+    
+    return(g)
+    
+}
+
---
+++ 00_Deployment/Cohort_Retention.Shiny.app.R
@@ -0,0 +1,239 @@
+# RShiny App: Cohort Retention Analyzer 
+
+# LIBRARIES ----
+library(shiny)
+library(shinyWidgets)
+library(bslib)
+library(crosstalk)
+library(shinycssloaders)
+ 
+# Core
+library(tidyverse)
+library(tidyquant)
+
+# Visual + Others
+library(plotly)
+library(timetk)
+library(glue)
+library(rlang)
+
+
+# SET-UP ----
+
+# Environment 
+setwd("/Users/seunghyunsung/Documents/GitHub/Retail-Customer-Analytics")
+
+# DATA 
+trans_invoice_tbl <- read_rds("00_Data/data_wranggled/trans_data/trans_invoice_tbl.rds")
+trans_cust_tbl <- read_rds("00_Data/data_wranggled/trans_data/trans_cust_tbl.rds")
+
+# Source Functions 
+source("00_Script/select_period_n_cohort_n_statusIII.R")
+
+
+# Interactive UI {Event Button}
+
+# Period List (Input User)
+periods <- c("month", "quarter")
+period_list_tbl <- tibble(period = periods) 
+
+# Depending on the UI(period) selected ---> list for UI(cohorts_label) 
+period_cohort_map_tbl  <- period_list_tbl %>% 
+  mutate(cohort_label = map(period, generator_cohort_list_mapper))
+
+
+ui <- fluidPage(
+  # footer
+  footer = tagList(
+    "Made by Seung Hyun Sung"
+  ),
+  # Theme
+  theme = bs_theme(version = 4, bootswatch = 'lux'),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  # Header
+  div(
+    h1("Cohort Analysis", "by Model Bakery"),
+    p("Cohort analysis allows you to dig into the behaviour of customers of an online retailer.")
+  ),
+  
+  sidebarLayout(
+    sidebarPanel(
+      width = 3,
+      h1("Retail Customer Cohort Heatmap"),
+      h3("by Model Bakery") %>% tags$a(href = 'https://modelbakery-academic.netlify.app', target="_blank"),
+      p("Cohort analysis allows you to segment customers based on when they first bought from the retailer, and see how many customers based on when they first bought from the retailer, and see how many customers remained after a set number of months.
+         For each cohort, you can see how many remained over time, and what was the average spend per transcation for those remaining customers."),
+      br(),
+      hr(),
+      h3("Period Controls"),
+      wellPanel(
+        pickerInput(
+          inputId = "period_selection", 
+          label = "Period List (Pick One to Analyse)",
+          choices = periods,
+          multiple = FALSE,
+          selected = "month",
+          options = pickerOptions(
+            actionsBox = FALSE
+          )
+        ),
+        actionButton(inputId = "analyse", label = "Analyse", icon = icon("download")),
+        verbatimTextOutput(outputId = "selected_period")
+      ),
+      h3("Cohort Controls"),
+      wellPanel(
+        pickerInput(
+          inputId = "period_selection1", 
+          label = "Period List (Pick One to Analyse)",
+          choices = NULL,
+          multiple = FALSE,
+          options = pickerOptions(
+            liveSearch = TRUE,
+            actionsBox = FALSE,
+            size = 5
+          )
+        ),
+        actionButton(inputId = "analyse1", label = "Analyse", icon = icon("download"))
+      ),
+      hr(),
+      p("Customer retention measures show how many new customers bought per month, as well as number of returning customers (customers who bought both last month and this month) and the recovered customers (customers who had a break from buying but have returned for another purchase)."),
+      p("Spend analysis shows the total and average spend transaction vary over time. Average distribution shows a histogram of customer and their average monthly spend amounts.")
+      
+      ),
+    mainPanel(
+      width = 9,
+      radioGroupButtons(
+        inputId = "cat",
+        label = NULL,
+        choices = c("Active Customers", 
+                    "Customers Retented",
+                    "Average Spend"
+        ),
+        justified = TRUE
+      ),
+      hr(style="margin-top:2px !important;"),
+      uiOutput("mainP")
+      
+      )
+    
+  )
+
+  
+  
+)
+
+server <- function(input, output, session) {
+  
+  r<-reactiveValues(per="month",per1="all")
+  
+  observeEvent(input$analyse,{
+    r$per<-input$period_selection
+    r$per1<-input$period_selection1
+  })
+  
+  observeEvent(input$analyse1,{
+    r$per<-input$period_selection
+    r$per1<-input$period_selection1
+  })
+  
+  observeEvent(input$period_selection,{
+    req(input$period_selection)
+    cohort_list <- period_cohort_map_tbl %>% 
+      filter(period == input$period_selection) %>% 
+      select(cohort_label) %>% unnest(cols = cohort_label)
+    
+    updatePickerInput(session = session,inputId = 'period_selection1',choices = cohort_list)
+  })
+  
+  
+  
+  output$mainP<-renderUI({
+    tagList(
+    h1("Retail Customer Cohort Heatmap ", tags$small(paste("BY",str_to_upper(input$cat)))),
+    plotlyOutput("plot1") %>%
+      withSpinner(),
+    h1("Customer Retention Status"),
+    fluidRow(
+      column(6,
+             plotlyOutput('plot2',height = '500px')%>%
+               withSpinner(type = 7)
+             ),
+      column(6,
+             plotlyOutput('plot3',height = '500px')%>%
+               withSpinner(type = 7)
+             )
+    ),
+    hr(),
+    fluidRow(
+      column(6,
+             plotlyOutput('plot4',height = '500px')%>%
+               withSpinner(type = 7)
+             ),
+      column(6,
+             plotlyOutput('plot5',height = '500px')%>%
+               withSpinner(type = 7)
+             )
+    ),
+    br(),
+    br()
+
+    )
+  })
+  
+  output$plot1<-renderPlotly({
+    req(input$cat)
+    g<-switch (input$cat,
+            "Active Customers" = {trans_invoice_tbl %>% abstract_by_period(period = r$per) %>% get_cohort_heatmap() %>% plot_heatmap(measure_var = active_users)},
+            "Customers Retented" = {trans_invoice_tbl %>% abstract_by_period(period = r$per) %>% get_cohort_heatmap() %>% plot_heatmap(measure_var = retention)},
+            "Average Spend" = {trans_invoice_tbl %>% abstract_by_period(period = r$per) %>% get_cohort_heatmap() %>% plot_heatmap(measure_var = avg_spend)}
+    )%>%
+      layout(
+             yaxis = list(title = 'Cohort Group', titlefont = list(size = 12) ),
+             xaxis = list(title = paste("k-th",r$per), titlefont = list(size = 12)),
+             showlegend=FALSE
+      )%>%
+      config(displayModeBar = F)
+  })
+  
+  output$plot2<-renderPlotly({
+    trans_invoice_tbl %>% abstract_by_period(period = r$per) %>% abstract_by_cohort(cohort_label = r$per1) %>% get_retention_by_status() %>% plot_retention_status()%>%
+      layout(legend = list(orientation = 'h',x = -0.2, y = 1.3),
+             title = list(y = 0.85),
+             yaxis = list(title = 'No. Customers', titlefont = list(size = 12) ),
+             xaxis = list(title = paste("Invoice by",r$per), titlefont = list(size = 12))
+             )%>%
+      config(displayModeBar = F)
+  })
+  output$plot3<-renderPlotly({
+    trans_invoice_tbl %>% abstract_by_period(period = r$per) %>% abstract_by_cohort(cohort_label = r$per1) %>% get_retention_by_status() %>% plot_retention_breakdown()%>%
+      layout(legend = list(orientation = 'h',x = -0.2, y = 1.3),
+             title = list(y = 0.85),
+             yaxis = list(title = 'No. Customers', titlefont = list(size = 12) ),
+             xaxis = list(title = paste("Invoice by",r$per), titlefont = list(size = 12))
+             )%>%
+      config(displayModeBar = F)
+  })
+  output$plot4<-renderPlotly({
+    trans_invoice_tbl %>% abstract_by_period(period = r$per) %>% abstract_by_cohort(cohort_label = r$per1) %>% plot_spending_trend()%>%
+      config(displayModeBar = F)%>%
+      layout(
+        legend = list(orientation = 'h',x = -0.2, y = 1.3),
+        yaxis = list(title = 'Total Spend', titlefont = list(size = 12) ),
+        xaxis = list(title = paste("Invoice by",r$per), titlefont = list(size = 12))
+      )
+  })
+  output$plot5<-renderPlotly({
+    trans_cust_tbl %>% abstract_cust_spend_by_period(r$per) %>% abstract_by_cohort(cohort_label = r$per1) %>% plot_spending_distribution()%>%
+      config(displayModeBar = F)%>%
+      layout(
+        legend = list(orientation = 'h',x = -0.2, y = 1.3),
+        yaxis = list(title = 'No. Customers', titlefont = list(size = 12) ),
+        xaxis = list(title = paste("Average",paste0(r$per,"ly"),"Spend"), titlefont = list(size = 12))
+      )
+  })
+
+}
+
+shinyApp(ui, server,options = list(port=1212))
